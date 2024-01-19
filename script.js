7888ecf8367a6f15bdc5af9c219e66ced69309e3
GabrielDevJourney <gabspereira178@gmail.com>
2024-01-19 18:06:12 +0000

    create main files, create function to create divs

---
+++ script.js
@@ -0,0 +1,7 @@
+function createDivs() {
+    let container = document.querySelector('.container');
+    let newdiv = document.createElement('div');
+    fo    
+    container.appendChild(newdiv);
+}
+createDivs();
