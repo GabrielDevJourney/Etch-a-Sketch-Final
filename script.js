dad6d81a74ef4f2520a2ecf232a74350176079e2
GabrielDevJourney <gabspereira178@gmail.com>
2024-01-19 18:44:44 +0000

    hover effect to change divs colors

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function createDivs() {
     let container = document.querySelector('.container');
-    let newdiv = document.createElement('div');
-    fo    
-    container.appendChild(newdiv);
+    let totaldivs = 16 * 16;
+    for (let i= 0; i < totaldivs; ++i) {   
+        let newdiv = document.createElement('div');
+        newdiv.className = 'divForGrid';
+        //*adding mouse hover effect that change color
+        newdiv.addEventListener('mouseenter', () => {
+            newdiv.style.backgroundColor = "black";
+        });
+        container.appendChild(newdiv);
+    }
 }
 createDivs();
